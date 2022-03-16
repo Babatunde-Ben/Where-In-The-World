679dbd52f6cbe4fc5a7f2ef0ca95a8f65919bb6a
Babatunde-Ben <babatundeben110@gmail.com>
2022-03-16 14:10:44 +0100

    filtering countries by region

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const body = document.querySelector("body");
 const section1 = document.querySelector(".section-1");
 const section2 = document.querySelector(".section-2");
 const mainContainer = document.querySelector(".countries");
+const options = document.querySelectorAll(".options");
+const option = document.querySelectorAll(".options .option");
 const detailSection = document.querySelector(".detail-section");
 const darkModeBtn = document.querySelector(".dark-mode-btn");
 const dropdown = document.querySelector(".dropdown");
@@ -9,6 +11,7 @@ const selectedInput = document.querySelector(".selected-dropdown");
 const searchBoxInput = document.querySelector(".search-box input");
 
 let url;
+let region;
 
 // toggle dark mode
 darkModeBtn?.addEventListener("click", () => {
@@ -34,23 +37,59 @@ dropdown?.addEventListener("click", () => {
   dropdown.classList.toggle("active");
 });
 
+// functionality for filtering countries container by region
+
+option.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (e.target.textContent == "All") {
+      selectedInput.value = "Filter by Region";
+      url = "https://restcountries.com/v2/all";
+      generateCountries(url);
+    } else {
+      selectedInput.value = e.target.textContent;
+      region = e.target.textContent;
+      url = `https://restcountries.com/v2/region/${region}`;
+      byRegion(url, region);
+    }
+  });
+});
+
 // populate countries for home page
 window.addEventListener("DOMContentLoaded", () => {
   url = "https://restcountries.com/v2/all";
   generateCountries(url);
 });
 
-// function to populate countries
+// function to populate all countries
 function generateCountries(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       const countryNameArray = data.map((item) => {
-        console.log(item);
-        //   return `<div class="country">
-        //   <p>${item.name}, </p>
-        //   <a href="/more_info.html?country=${item.name}">see more details</a>
-        // </div>`;
+        return ` <div class="country">
+      <img src="${item.flags.png}" alt="${item.name}" />
+      <div class="country-details">
+        <h3>${item.name}</h3>
+
+        <p><b>population:</b> ${item.population}</p>
+        <p><b>region:</b> ${item.region}</p>
+        <p><b>capital:</b> ${item.capital}</p>
+      </div>
+    </div>`;
+      });
+      mainContainer.innerHTML = countryNameArray.join("");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// function to populate countries by region
+function byRegion(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const countryNameArray = data.map((item) => {
         return ` <div class="country">
       <img src="${item.flags.png}" alt="${item.name}" />
       <div class="country-details">
